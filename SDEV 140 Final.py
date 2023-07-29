ed49042fdc83b33795678239aada95d71e19669d
Hyperion317 <139717113+Hyperion317@users.noreply.github.com>
2023-07-29 13:35:27 -0400

    Add files via upload

--- SDEV 140 Final.py
+++ SDEV 140 Final.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk,Image
 import random
 
 
@@ -6,9 +7,9 @@ import random
 class Sleeves_App:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("670x570+0+0")
-        self.root.title("Knee Sleeves")
-        title = Label(self.root, text = "Knee Sleeves", font = ('times new roman', 30, 'bold'), pady = 2, bd = 12, bg = "#07B4FF", fg = "Black", relief = GROOVE)
+        self.root.geometry("1005x570+0+0")
+        self.root.title("Hyperion Knee Sleeves")
+        title = Label(self.root, text = "Hyperion Knee Sleeves", font = ('times new roman', 30, 'bold'), pady = 2, bd = 12, bg = "#07B4FF", fg = "Black", relief = GROOVE)
         title.pack(fill = X)
         
     # ================SBD Sleeve=======================
@@ -16,7 +17,9 @@ class Sleeves_App:
       
     # ============Inzer Sleeve==============================
         self.ergo = IntVar()
-       
+
+    # ============Iron Rebel===========
+        self.ironpr = IntVar()
     
     # ==============Customer==========================
         self.name = StringVar()
@@ -35,6 +38,17 @@ class Sleeves_App:
         
         name_txt = Entry(F1, width = 15, textvariable = self.name, font = 'arial 15', bd = 7, relief = GROOVE)
         name_txt.grid(row = 0, column = 1, pady = 5, padx = 10)
+
+
+    # ========Photo=============
+        def open():
+            global sbd
+            top = Toplevel()
+            top.title('SBD Phantom')
+            sbd_img = ImageTk.PhotoImage(Image.open("sbd.jpg"))
+            sbd_lbl = Label(top, image = sbd_img).pack()
+            sbd2_btn = Button(top, text = "Close Window", command = top.destroy).pack()
+    
         
     # ===================SBD====================================
         F2 = LabelFrame(self.root, text = "SBD", font = ('times new roman', 15, 'bold'), bd = 10, fg = "Black", bg = "#07B4FF")
@@ -46,10 +60,7 @@ class Sleeves_App:
         
         phan_btn = Button(F2, width = 10, command = self.phan, text = ("SBD Sleeve"))
         phan_btn.grid(row = 0, column = 1, padx=10, pady=10)
-
-    # ========Photo=============
         
-
 
     # ==========Inzer=========================
         F3 = LabelFrame(self.root, text = "Inzer", font = ('times new roman', 15, 'bold'), bd = 10, fg = "Black", bg = "#07B4FF")
@@ -61,6 +72,18 @@ class Sleeves_App:
         
         ergo_btn = Button(F3, width=10, command = self.ergo, text = ('Ergo Sleeve'))
         ergo_btn.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+
+    # ==========Iron Rebel=========================
+        F4 = LabelFrame(self.root, text = "Iron Rebel", font = ('times new roman', 15, 'bold'), bd = 10, fg = "Black", bg = "#07B4FF")
+        F4.place(x = 675, y = 180, width = 325, height = 380)
+        
+    # ===============Iron Rebel Button============
+        pr_lbl = Label(F4, text = "Iron Rebel; PR", font = ('times new roman', 16, 'bold'), bg = "#07B4FF", fg = "black")
+        pr_lbl.grid(row = 0, column = 0, padx = 10, pady = 10)
+        
+        pr_btn = Button(F4, width=10, command = self.ironpr, text = ('PR Sleeve'))
+        pr_btn.grid(row = 0, column = 1, padx = 10, pady = 10)
 
 
 
